fb679527ca13a2b6d70a09646dc0ce9322925c4f
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-11-13 11:57:23 -0300

    Added edit comments

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -25,6 +25,28 @@ router.post('/', isLoggedIn , (req, res)=>{
     });
 });
 
+router.get('/:comment_id/edit', (req, res)=>{
+    Comment.findById(req.params.comment_id, (err, foundComment)=>{
+        if(err){console.log(err); res.redirect('back')};
+        res.render('comments/edit', {campground:req.params.id, comment:foundComment});
+    });
+    
+});
+
+router.put('/:comment_id', (req, res)=>{
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment)=>{
+        if(err){console.log(err); res.redirect('back')};
+        res.redirect(`/campgrounds/${req.params.id}`)
+    });
+});
+
+router.delete('/:comment_id', (req, res)=>{
+    Comment.findByIdAndDelete(req.params.comment_id, (err)=>{
+        if(err){console.log(err); res.redirect('back')};
+        res.redirect(`/campgrounds/${req.params.id}`)
+    })
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return(next());
