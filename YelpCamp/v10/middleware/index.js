37473f29ffd07fdeb82925c3e5e5c7019c785bc6
Mateus Felipe <coall.fcm@gmail.com>
2019-11-24 12:44:17 -0300

    Create index.js

---
+++ YelpCamp/v10/middleware/index.js
@@ -0,0 +1,45 @@
+const middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, (err, foundCampground)=>{
+            if(err){console.log(err); res.redirect('back')}
+            if(foundCampground.author.id.equals(req.user._id)){
+                next()
+            }
+            else{
+                res.redirect('back');
+            }
+        });
+    }
+    else{
+        res.redirect('back');
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, (err, foundComment)=>{
+            if(err){console.log(err); res.redirect('back')}
+            if(foundComment.author.id.equals(req.user._id)){
+                next()
+            }
+            else{
+                res.redirect('back');
+            }
+        });
+    }
+    else{
+        res.redirect('back');
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return(next());
+    }
+    res.redirect('/login');
+}
+
+
+module.exports = middlewareObj;
